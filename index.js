3e608bd809415edfd7321378b038a03a527b821f
ecman <ecman@users.noreply.github.com>
2017-04-25 23:12:30 -0700

    Add core functionality
    Fill out the stub script

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
+'use strict';
 module.exports = flatible;
 
-function* flatible() {
+function* flatible(thing, index) {
+  if (thing instanceof Array) {
+    index = index || 0;
+    while (index < thing.length) {
+      yield* flatible(thing[index++], null);
+    }
+  }
+  else if (index !== undefined) {
+    yield thing;
+  }
 }
+
