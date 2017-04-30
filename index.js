e8591ac2bbcce3c7c935543f7bc9237320641e9d
ecman <ecman@users.noreply.github.com>
2017-04-30 00:13:56 -0700

    Fix RangeError with circular references
    Use WeakSet to track references

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 'use strict';
 module.exports = flatible;
 
-function* flatible(thing, index) {
+function* flatible(thing, index, seen) {
+  seen = seen || new WeakSet();
   if (thing instanceof Array) {
+    if (seen.has(thing)) return;
+    seen.add(thing);
     index = index || 0;
     while (index < thing.length) {
-      yield* flatible(thing[index++], null);
+      yield* flatible(thing[index++], null, seen);
     }
   }
   else if (index !== undefined) {
     yield thing;
   }
 }
-
