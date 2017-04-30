e957d3819cc3b9b3af6d78a6951d54e3bd4a4a5f
ecman <ecman@users.noreply.github.com>
2017-04-30 00:44:40 -0700

    Reduce branching in core function

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ module.exports = flatible;
 
 function* flatible(thing, index, seen) {
   seen = seen || new WeakSet();
-  if (thing instanceof Array) {
-    if (seen.has(thing)) return;
+  let skip = seen.has(thing);
+  if (!skip && thing instanceof Array) {
     seen.add(thing);
     index = index || 0;
     while (index < thing.length) {
       yield* flatible(thing[index++], null, seen);
     }
   }
-  else if (index !== undefined) {
+  else if (!skip && index !== undefined) {
     yield thing;
   }
 }
